c7fe1934392c27f294abc53ad54a5eba1b395472
kshitiz sinha <kshtzkr@gmail.com>
2026-04-17 16:51:56 +0530

    red(user): require an email
    First behaviour we need from User: an account without an email is not
    valid. The spec drives the existence of the model and its email column.
    
    Run: bundle exec rspec spec/models/user_spec.rb
    Expected failure: NameError: uninitialized constant User

---
+++ api/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it "is invalid without an email" do
+      user = User.new(email: nil)
+
+      user.valid?
+
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
