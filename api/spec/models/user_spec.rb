fc3c13bb177bea0797237f299b954365631986c3
kshitiz sinha <kshtzkr@gmail.com>
2026-04-17 16:52:20 +0530

    red(user): reject duplicate emails
    A second account with the same email must not be valid. The next green
    commit will add a uniqueness validator and the supporting unique index.

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -9,5 +9,14 @@ RSpec.describe User, type: :model do
 
       expect(user.errors[:email]).to include("can't be blank")
     end
+
+    it "is invalid when the email is already taken" do
+      User.create!(email: "duplicate@salary.local")
+      user = User.new(email: "duplicate@salary.local")
+
+      user.valid?
+
+      expect(user.errors[:email]).to include("has already been taken")
+    end
   end
 end
