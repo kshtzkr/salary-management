dff2400f3c361b151b8e3c926ae1adc3fcba5e20
kshitiz sinha <kshtzkr@gmail.com>
2026-04-17 16:52:30 +0530

    green(user): enforce email uniqueness with validator and unique index
    - migration adds a unique index on users.email
    - User adds 'uniqueness: true' to the email validator
    
    Both halves matter: validator gives a friendly error, the unique index
    prevents races at the DB level.

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -3,5 +3,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_16_060000) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
