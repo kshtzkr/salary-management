a9f05f7a37a2c94e07aa87552a6b242cbed4d5f8
kshitiz sinha <kshtzkr@gmail.com>
2026-04-18 09:46:53 +0530

    green(insights): authorize_read_access! + require_country! guards
    Two small before_actions: predicate check via can_view_insights? and
    a param guard that keeps SalaryInsights from ever receiving blank
    country input.

--- api/app/controllers/api/v1/insights_controller.rb
+++ api/app/controllers/api/v1/insights_controller.rb
@@ -1,8 +1,25 @@
 module Api
   module V1
     class InsightsController < ApplicationController
+      before_action :authorize_read_access!
+      before_action :require_country!
+
       def overview
         render json: SalaryInsights.new(country_code: params[:country]).overview
+      end
+
+      private
+
+      def authorize_read_access!
+        return if current_user.can_view_insights?
+
+        render_error("You are not allowed to view salary insights", :forbidden)
+      end
+
+      def require_country!
+        return if params[:country].present?
+
+        render_error("country is required", :unprocessable_entity)
       end
     end
   end
